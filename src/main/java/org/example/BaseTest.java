57640d38642ba06990544217c74101646a1cf8fa
Cory Max Bernhardt <corymaxbernhardt@gmail.com>
2023-03-28 22:40:13 -0700

    notes
    added a sleep that was throwing an exception so we put it in a try catch flow to
    
    throw new will intentionally throw an exeption

--- src/main/java/org/example/BaseTest.java
+++ src/main/java/org/example/BaseTest.java
@@ -8,14 +8,17 @@ import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+
 import java.net.URL;
+import java.util.List;
 
 public class BaseTest {
     AndroidDriver driver;
-
+    String login = "iamlearningautomoation";
+    String password = "Directv1";
     @BeforeTest
     public void setUp() throws Exception {
-        // Desiredcapabilities type of object and capabilities is the name of the object = to the right
+        // Desired capabilities type of object and capabilities is the name of the object = to the right
         // is creating a new object of DesiredCapabilites
         DesiredCapabilities capabilities = new DesiredCapabilities();
         String name = "";
@@ -27,7 +30,11 @@ public class BaseTest {
         capabilities.setCapability("appPackage", "com.google.android.gm");
         capabilities.setCapability("appActivity", "com.google.android.gm.ui.MailActivityGmail");
         capabilities.setCapability("appium:automationName", "uiautomator2");
+        //full reset
+        //capabilities.setCapability("fullReset", true);
 
+        //reset after test capability
+        //capabilities.setCapability("fullReset", true);
 
 
         driver = new AndroidDriver(new URL("http://0.0.0.0:4723/wd/hub"), capabilities);
@@ -36,8 +43,8 @@ public class BaseTest {
 
 
     @Test
-    void blankTest() {
-       // Does something now change comment
+    void blankTest() throws InterruptedException {
+        // Does something now change comment
         WebElement newInGmail = driver.findElement(By.id("com.google.android.gm:id/welcome_tour_title"));
         WebElement allTheFeatures = driver.findElement(By.id("com.google.android.gm:id/welcome_tour_text"));
         WebElement gotIt = driver.findElement(By.id("com.google.android.gm:id/welcome_tour_got_it"));
@@ -45,6 +52,38 @@ public class BaseTest {
         Assert.assertEquals(allTheFeatures.getText(), "All the features you love with a fresh new look");
         Assert.assertEquals(gotIt.getText(), "GOT IT");
         gotIt.click();
+        try {
+            // try is to catch
+            Thread.sleep(500);
+            //   System.out.println("This is before throwing a new exception");
+            //   throw new Exception("Uncaught Exception sleep throws and why we put it in try catch");
+            // below is a webelement that could not be found so it threw an exception to be caught
+            // WebElement addAnEmailAddress = driver.findElement(By.id("com.google.android.gm:id/setup_addresses_add_another"));
 
+        } catch (Exception e) {
+            // will only ever get into a catch block if an exception is thrown. not everytime an exception is thrown
+           System.out.println("Caught interrupted exception that can be thrown from sleep in this example");
+         //   throw new Exception("Failed to find id Add an email address");
+
+        }
+        // Thread.sleep(500);
+
+        // driver.manage().timeouts().implicitlyWait(60);
+        // wait.until(Assert.assertEquals(""););
+        // Page 2 after selecting Got it
+        // need to figure out wait
+        //wait(7000);
+        // WebElement youCanNow = driver.findElement(By.id("com.google.android.gm:id/setup_addresses_title"));
+        //  Assert.assertEquals(youCanNow.getText(), "You can now add all your email addresses. Learn more");
+         WebElement addAnEmailAddress = driver.findElement(By.id("com.google.android.gm:id/setup_addresses_add_another"));
+         Assert.assertEquals(addAnEmailAddress.getText(), "Add an email address");
+         addAnEmailAddress.click();
+         Thread.sleep(500);
+         List<WebElement> emailProviders = driver.findElements(By.id("com.google.android.gm:id/account_setup_label"));
+         emailProviders.get(0).click();
+        // A
+
+        // String login = login, "iamlearningautomoation";
+        //String password = "Directv1";
     }
 }
